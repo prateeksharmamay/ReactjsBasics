aacd704f8c4848eafcfb70746c848936f112578b
Prateek Sharma <prateek_sharma_may@yahoo.com>
2017-04-18 23:51:03 -0700

    Component Creation

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,21 @@
 /**
  * Created by Prateek on 4/17/2017.
  */
-console.log("It Works");
+import React from "react";
+import {render} from "react-dom";
+
+class App extends React.Component{
+    // render () - called by react whenever it feels it has to render the component
+    render(){
+        // return what should be rendered
+        // This is JSX - Javascript mixed with XML/HTML
+        // Following code is equivalent: to
+        // document.createElement("div");
+        // Following code is convenient way to do above command.
+        return(
+            <div>
+                <h1>hello!</h1>
+            </div>
+        );
+    }
+}
