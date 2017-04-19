abdf13e7e31630bd6e884bab6d6ed67e818b70d6
Prateek Sharma <prateek_sharma_may@yahoo.com>
2017-04-19 13:12:04 -0700

    output dynamic data

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -5,9 +5,21 @@ import React from 'react';
 
 export class Home extends React.Component{
     render(){
+        let content = "";
+        let para =  "";
+
+        if(true){
+            content = "Aloha";
+            para = <p>New Para</p>;
+        }
+
         return(
             <div>
                 <p>In a New Component</p>
+                {2+1}
+                {content}
+                {para}
+                {8 == 3 ? "Yes" : "No"}
             </div>
         );
     }
