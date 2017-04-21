f7560c8c0a15b522cd279498d109a0efd15e48e8
Prateek Sharma <prateek_sharma_may@yahoo.com>
2017-04-20 21:05:49 -0700

    Changed Header to stateless component

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,7 +2,25 @@
  * Created by Prateek on 4/19/2017.
  */
 import React from 'react';
+// For components, that do not require to deal with states.
+// We should create them as Stateless Components
 
+export const Header = (props) => {
+    return(
+        <nav className="navbar navbar-default">
+            <div className="container">
+                <div className="navbar-header">
+                    <ul className="nav navbar-nav">
+                        <li><a href="#">{props.homeLink}</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+/*
+// Used Till We dont needed stateless components
 export class Header extends React.Component{
     render(){
         return(
@@ -17,4 +35,4 @@ export class Header extends React.Component{
            </nav>
         );
     }
-}
+}*/
